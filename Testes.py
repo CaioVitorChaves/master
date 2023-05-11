4eebc7426486257aaa2588fa29ac4f1c6498dc14
CaioVitorLChaves <42558079+CaioVitorChaves@users.noreply.github.com>
2023-05-11 01:34:32 -0300

    Add files via upload

---
+++ Testes.py
@@ -0,0 +1,192 @@
+# nota1 = float(input("Digite sua primeira nota: "))
+# nota2 = float(input("Digite sua segunda nota: "))
+# nota3 = float(input("Digite sua terceira nota: "))
+
+# media = ((nota1 + nota2 + nota3) / 3)
+
+# if (media >= 7 ):
+#     print("Aprovado")
+
+# else:
+#     print("Reprovado")
+
+# a = 7==7
+# print(type (a))
+
+# nome = "caio"
+# sobrenome = "vitor"
+# sobrenome1 = "lopes"
+# sobrenome2 = "chaves"
+# print (nome, sobrenome, sobrenome1, sobrenome2)
+
+# valor1 = float(input("Digite seu primeiro valor: "))
+# valor2 = float(input("Digite seu segundo valor: "))
+# total = (valor1 + valor2)
+
+# print ('Seu valor total é: ',total, 'reais')
+
+# a = 5
+# b = 5
+# print(a + b, a - b, a * b, a / b)
+
+# a = 3+7
+# resultado = a==10
+# print(a)
+# print(resultado)
+
+# a = 5 + 5 \
+# + 5
+
+# print(a)
+
+# a = 'Caio'
+# print(a[0])
+
+# if 10 != 88/3:
+#     print('verdade')
+
+# if 10 == 40/4:
+#     print('verdade')
+# if 10 == 88/3:
+#     print('verdade')
+
+# else: 
+#     print('mentira')
+
+# def compare_com_oito(y):
+#     if y > 8:
+#         return 'Maior'
+    
+#     elif y < 0:
+#         return 'Negativo'
+    
+#     elif y < 8:
+#         return'Menor'
+    
+    
+#     else:
+#         return 'Igual'
+# print(compare_com_oito(8))
+
+# def adicione_15(x):
+#     if x >= 100:
+#         return(x + 15)
+#     else:
+#        return("Não ganhou nada")
+    
+# print(adicione_15(90))
+
+# def aprendi():
+#     print('minha primeira função')
+
+# aprendi()
+
+# def mais_nove(a):
+#     return (a + 9)
+
+# print(mais_nove(1000))
+
+# def conta(a,b,c):
+#     resultado = a - b * c
+#     print('paramentro a é igual', a)
+#     print('paramentro b é igual', b)
+#     print('paramentro c é igual', c)
+#     return resultado
+# print(conta(2,2,2))
+
+# maior_da_lista = [887,18,4]
+# print(max(maior_da_lista))
+
+# z = -20
+# abs(z)
+# print(abs(z))
+
+# lista = (10,10,80)
+# print(sum(lista))
+
+# a = (round(3.585265456, 2))
+# print(a)
+
+# a = pow(2,10)
+# print(a)
+
+# a = len('Jesus')
+# print(a)
+
+# def salario(hrs_trab):
+#     return hrs_trab * 40
+
+# def com_bonus(hrs_trab):
+#     return salario(hrs_trab) + 50
+# print(salario(8), com_bonus(8))
+
+# hrs_trab = 5
+# salario = hrs_trab * 40 
+# if hrs_trab > 8:
+#     print(salario + 50, 'Voce ganhou o salario + o bonus de 50')
+# else:
+#     print( 'Voce trabalhou as 8 horas ou menos')
+    
+# def compare_as_duas(x, y):
+#     if x > y:
+#         print("Maior")
+#     elif y > x:
+#         print("Menor")
+#     else:
+#         print("Iguais")
+# print(compare_as_duas(11, 11))
+
+# def multi_por_7(x):
+#    return x * 7
+# print(multi_por_7(7))
+
+#convidados = ['Caio','Vitor', 'Lopes', 'Chaves']
+# # print(convidados[0])
+# # print(convidados[-1])
+# # convidados[0] = 'Vitor'
+# del convidados[0]
+# print(convidados)
+#convidados.append('Chaves')
+#convidados.extend(['Vitor', 'Lopes'])
+#print(convidados)
+#print('O primeiro nome é:',convidados[0])
+# print(convidados[1:3])
+# print(convidados[:2])
+# print(convidados[1:])
+# print(convidados[-3:])
+# print(convidados.index('Lopes'))
+#novos = ['Deus', 'Jesus', 'Cristo']
+#print(novos)
+#vida = [convidados, novos]
+#print(vida)
+# novos.sort()
+# print (novos)
+# convidados.sort()
+# print(convidados)
+# vida.sort(reverse=True)
+# print(vida)
+# numeros = [1,2,3,4,5,6,7,8,9,10]
+# numeros.sort(reverse=True)
+# print(numeros)
+
+# nome, idade = 'Caio, 24'.split(',')
+# print(nome)
+# print(idade)
+
+#dicionario = {'c2': 'Caio', 'v1': 'Vitor', 'd1': 'Deus','j1' : 'Jesus'}
+#print(dicionario['d1'])
+#dicionario['c1'] = 'Cristo'
+#dicionario['c2'] = 'caio'
+#print(dicionario)
+
+# dep_trabalhadores = {'dep_1': 'caio', 'dep_2': ['vitor', 'lopes', 'chaves']}
+# print(dep_trabalhadores['dep_2'])
+
+# time = {}
+# time ['golerio'] = 'caio'
+# time ['zaguerio'] = 'vitor'
+# time ['meia'] = 'lopes'
+# time ['atacante'] = 'chaves'
+# print(time['meia'])
+# print(time.get('atacante'))
+
